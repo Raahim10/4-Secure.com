c6607191de86ab62c4bb03fb0db91486d73d3acd
Raahim10 <142586661+Raahim10@users.noreply.github.com>
2023-08-18 15:28:33 +0500

    Add files via upload

---
+++ nav-btns.js
@@ -0,0 +1,11 @@
+burger = document. querySelector (' .burger')
+navbar = document.querySelector (' .navbar')
+navbtn = document.querySelector (' .nav-btn')
+navoption = document.querySelector (' .navoption')
+
+
+
+burger. addEventListener('click', ()=>{
+navoption.classList.toggle('nav-mobile');
+
+})
